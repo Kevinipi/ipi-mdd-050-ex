60f8c8ce732795de5581339c77a2d0e31de8799a
Kevinipi <kevin.pose@ipilyon.net>
2020-01-03 15:12:47 +0100

    Answers question 2 in exercises

--- src/main/java/com/ipiecoles/java/mdd050/controller/EmployeController.java
+++ src/main/java/com/ipiecoles/java/mdd050/controller/EmployeController.java
@@ -1,8 +1,10 @@
 package com.ipiecoles.java.mdd050.controller;
 
+import com.ipiecoles.java.mdd050.model.Employe;
 import com.ipiecoles.java.mdd050.repository.EmployeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,9 +14,14 @@ public class EmployeController {
     @Autowired // Un repository existe et j'ai besoin de l'utiliser
     private EmployeRepository employeRepository;
 
-    @RequestMapping("/count") //concaténation de l'URL d'en haut
+    @RequestMapping(value = "/count", method = RequestMethod.GET) //concaténation de l'URL d'en haut
     public long countEmployes(){
         //Récupérer le nombre d'employés et l'envoyer au client
         return employeRepository.count();
+    }
+
+    @RequestMapping(value="/6", method = RequestMethod.GET)
+    public Employe retreiveEmployes() {
+        return employeRepository.findById(6L).get();
     }
 }
