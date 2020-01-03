56e14be811fa1c7e12e1ce04b4c49d149447c211
Kevinipi <kevin.pose@ipilyon.net>
2020-01-03 15:31:32 +0100

    Ajout methode affichage dynamique employé

--- src/main/java/com/ipiecoles/java/mdd050/controller/EmployeController.java
+++ src/main/java/com/ipiecoles/java/mdd050/controller/EmployeController.java
@@ -3,6 +3,7 @@ package com.ipiecoles.java.mdd050.controller;
 import com.ipiecoles.java.mdd050.model.Employe;
 import com.ipiecoles.java.mdd050.repository.EmployeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,9 +20,17 @@ public class EmployeController {
         //Récupérer le nombre d'employés et l'envoyer au client
         return employeRepository.count();
     }
-
+/*
     @RequestMapping(value="/6", method = RequestMethod.GET)
     public Employe retreiveEmployes() {
         return employeRepository.findById(6L).get();
     }
+
+ */
+//Rendre dynamique l'affichage des employés
+    @RequestMapping(value="/{id}", method = RequestMethod.GET)
+    public Employe retrieveEmployes(@PathVariable ("id") Long id) {
+        return employeRepository.findById(id).get();
+    }
+
 }
