d09f961c5532e9bdc5f5ac03cc9603fe397ea8d5
Kevinipi <kevin.pose@ipilyon.net>
2020-02-10 12:02:04 +0100

    Import du RestController et du requestMapping concernant managers

--- src/main/java/com/ipiecoles/java/mdd050/controller/ManagerController.java
+++ src/main/java/com/ipiecoles/java/mdd050/controller/ManagerController.java
@@ -1,4 +1,9 @@
 package com.ipiecoles.java.mdd050.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/managers")
 public class ManagerController {
 }
